605bd50c17714bcd4916df35f33a2aef43ebe002
Chulkyun Park <chulkyun.p@gmail.com>
2016-12-31 17:32:12 +0900

    First Practice

--- Module.py
+++ Module.py
@@ -12,7 +12,7 @@ from IPython.display import display, Image
 from scipy import ndimage
 from sklearn.linear_model import LogisticRegression
 from six.moves.urllib.request import urlretrieve  # This code runs in IPython
-from six.moves import _compat_pickle as pickle    # This code runs in IPython
+from six.moves import cPickle as pickle    # This code runs in IPython
 
 %matplotlib inline
 
@@ -27,21 +27,21 @@ def download_progress_hook(count, blockSize, totalSize):
 	if last_percent_reported != percent:
 		if percent % 5 = 0:
 			sys.stdout.write("%s%%" % percent)
-			sys.stdout.flush()
+			sys.stdout.flush()									# What is the difference with flush and print?
 		else:
 			sys.stdout.write(".")
 			sys.stdout.flush()
 
 		last_percent_reported = percent
 
-def maybe_download(filename, expected_bytes, force=False):
+def maybe_download(filename, expected_bytes, force=False): 							# What is 'force'?
 # Download a file, make sure it's the right size
 	if force or not os.path.exists(filename):
-		print('Attempting to dwnload:', filename)
+		print('Attempting to download:', filename)
 		filename, _ = urlretrieve( url + filename, filename, reporthook = download_progress_hook)
 		print('\nDownload Complete!')
-	statinfo = os.stat(filename)
-	if statinfo.st_size == expected_bytes:
+	statinfo = os.stat(filename) 										# Scan File status 
+	if statinfo.st_size == expected_bytes:									# statinfo.st_size = file size
 		print('Found and verified', filename)
 	else:
 		raise Exception('Failed to verify ' + filename + '. Can you get to it with a browser?')
